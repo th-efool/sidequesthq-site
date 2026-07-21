48877170beb4e945f3277ab0d754f7594e5feb09
XMavrik <asas05202415@gmail.com>
2026-07-22 00:30:04 +0530

    Init LandingPage

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,17 @@
+import {Hero} from "@/src/client/components/screens/landing/01-hero"
+import {Ikigai} from "@/src/client/components/screens/landing/02-ikigai";
+import {Problem} from "@/src/client/components/screens/landing/03-problem";
+import {Community} from "@/src/client/components/screens/landing/04-community";
+import {Features} from "@/src/client/components/screens/landing/05-Features"
+import {Footer} from "@/src/client/components/screens/landing/06-footer";
+
 export default function landing(){
-    return <>LandingPage</>
+    return <>
+        <Hero/>
+        <Ikigai/>
+        <Problem/>
+        <Community/>
+        <Features/>
+        <Footer/>
+    </>
 }
---
+++ src/client/components/screens/landing/02-ikigai/index.ts
@@ -0,0 +1,1 @@
+export {Ikigai} from "./Ikigai"
