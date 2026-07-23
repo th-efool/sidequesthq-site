b621882b0fe772ab3397aa6a9fc542283307cafc
XMavrik <asas05202415@gmail.com>
2026-07-24 02:03:34 +0530

    Section2 (1/3) done

--- src/client/components/screens/landing/02-ikigai/Ikigai.tsx
+++ src/client/components/screens/landing/02-ikigai/Ikigai.tsx
@@ -1,9 +1,29 @@
-export function Ikigai(){
-    return <section
-        id="ikigai"
-        className="relative min-h-screen overflow-hidden"
-    >
+import { Section } from "@/src/client/components/global/layout/Section";
+import { IkigaiTimeline } from "./ikigaiTimeline";
+import styles from "./ikigai.module.css";
+import {Container} from "@/src/client/components/global/layout/Container";
 
+export function Ikigai() {
+    return (
+        <Section
+            spacing="xs"
+            className={styles.ikigai}
+        >
+            <Container size="full">
+                <IkigaiTimeline />
+            </Container>
 
-    </section>
+            <div className={styles.featureSection}>
+                {/* Video */}
+                {/* Copy */}
+                {/* Phone Illustration */}
+            </div>
+
+            <div className={styles.progressSection}>
+                {/* Learning List */}
+                {/* Calendar */}
+                {/* Progress Card */}
+            </div>
+        </Section>
+    );
 }
