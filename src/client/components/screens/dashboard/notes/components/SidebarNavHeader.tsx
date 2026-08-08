90b7af697ac3256984d5f80eab29ca664fc39ae6
th-efool <asas05202415@gmail.com>
2026-08-09 02:13:40 +0530

    (3/4 - create dedicated sidebar nav header component for notes)

---
+++ src/client/components/screens/dashboard/notes/components/SidebarNavHeader.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+export { SidebarNavHeader } from './NotesComponents';
+export type { SidebarNavHeaderProps } from './NotesComponents';
+export type SidebarTab = 'explorer' | 'search' | 'bookmarks';
+
